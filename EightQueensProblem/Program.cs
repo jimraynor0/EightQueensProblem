fdab4b9cf4d55558329cc289bbb7aaf153e20a97
weishui <kevin.feng@hotmail.com>
2020-04-17 19:39:02 +0800

    bugfixed

--- EightQueensProblem/Program.cs
+++ EightQueensProblem/Program.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EightQueensProblem;
 
 namespace EightQueensProblem
 {
     class Program
     {
-
+        private static void PrintSolutions(List<int[]> solutions, int size)
+        {
+            foreach (var item in solutions)
+            {
+                Console.WriteLine(string.Join(" ", item));
+            }
+            Console.WriteLine("尺寸为" + size + "的棋盘共有 " + solutions.Count + " 个解。");
+        }
 
         static void Main(string[] args)
         {
@@ -16,7 +24,7 @@ namespace EightQueensProblem
             {
                 Console.WriteLine("请输入棋盘的尺寸数字");
                 int problemSize = Convert.ToInt32(Console.ReadLine());
-                var engine = new EightQueensProblem.EightQueensProblem(problemSize);
+                var engine = new EightQueensProblem(problemSize);
                 PrintSolutions(engine.findSolutions(), problemSize);
             }
         }
